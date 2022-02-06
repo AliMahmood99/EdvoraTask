7e3fe2e6875c800a129cd43bcb206a7d0132a8ad
Alielfar <alielfar91@gmail.com>
2022-02-06 03:04:04 +0200

    Initial Commit

---
+++ EdvoraTask/EdvoraTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraTaskApp.swift
+//  EdvoraTask
+//
+//  Created by ali mahmood saad on 06/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
